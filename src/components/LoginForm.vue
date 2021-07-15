7c54b5d430be95b6c0e244d7510176ad7736d1d5
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-07-15 17:54:11 +0900

    6-4が完成

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>ログイン</h2>
+  <form>
+    <input type="email" required placeholder="メールアドレス" v-model="email">
+    <input type="password" required placeholder="パスワード" v-model="password">
+    <div class="error">{{ error }}</div>
+    <button>ログインする</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
