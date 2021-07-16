ec382048d8cfc39509d52c986a2c791c0a0ab357
Momoko Toyota <toyopet-momoko@hotmail.co.jp>
2021-07-16 21:54:57 +0900

    完了

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>チャットルームです</div>
+  <div class="container">
+    <navbar />
+  </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
+
 export default {
+  components: { Navbar },
 }
 </script>
 
-<style lang="css" scoped>
+<style scoped>
 </style>
